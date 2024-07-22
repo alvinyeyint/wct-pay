5adc22f29f68c96c814fbbb338bd9c4b751b4988
ye yint <yeyint@better.hr>
2024-07-22 21:19:05 +0700

    fixed: button text color

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -11,11 +11,11 @@ const props = defineProps({
   class: {
     type: String,
     required: false,
-    default: "",
+    default: "text-white",
   },
 });
 
 const className = computed(() => {
-  return `"relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-primary-600 shadow-sm hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500" ${props.class}`;
+  return `"relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md bg-primary-600 shadow-sm hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500" ${props.class}`;
 });
 </script>
